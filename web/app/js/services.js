f0bb5f9eeec4e94523f317f48b4d95a1328d4f52
Manuela Ticudean <manuelamaria@gmail.com>
2014-02-24 13:24:07 +0200

    use symfony production environment for REST requests

--- web/app/js/services.js
+++ web/app/js/services.js
@@ -1,6 +1,6 @@
 angular.module('competitionsServices', ['ngResource']).
     factory('Competition', function($resource){
-        return $resource('app_dev.php/competition/list/:filter', {}, {
+        return $resource('app.php/competition/list/:filter', {}, {
             query: {method:'GET', params:{}, isArray:true}
         });
 });
